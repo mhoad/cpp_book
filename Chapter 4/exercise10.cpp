c32c3ef01131a3a45d86340ef58553d5f050a995
Mark Hoad <markdhoad@gmail.com>
2013-02-16 11:46:50 +1100

    Chapter 4 exercise 10

---
+++ Chapter 4/exercise10.cpp
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ C++ Programming - From Problem Analysis to Program Design 5th Edition
+ Chapter 4 - Exercise 10
+ markdhoad@gmail.com
+ 
+ Redo Chapter 4 Exercise 9 to handle floating-point numbers. 
+ (Format your output to two decimal places.)
+ *****************************************************************************/
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    float numerator, denominator;
+    char operation;
+    
+    cout << "Enter your calculation: ";
+    cin >> numerator >> operation >> denominator;
+    
+    cout << fixed << showpoint << setprecision(2);
+    
+    switch (operation) {
+        case 'x':
+        case '*':
+            cout << numerator * denominator << endl;
+            break;
+        case '+':
+            cout << numerator + denominator << endl;
+            break;
+        case '-':
+            cout << numerator - denominator << endl;
+            break;
+        case '/':
+        case '%':
+            if (numerator == 0 || denominator == 0) {
+                cout << "Cannot divide by zero" << endl;
+                return 1;
+            }
+            cout << numerator / denominator << endl;
+            break;
+        default:
+            cout << "There was an error with your input" << endl;
+            return 1;
+            break;
+    }
+    
+    return 0;
+}
